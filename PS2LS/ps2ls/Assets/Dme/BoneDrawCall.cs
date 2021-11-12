b95034822487db2de3417233a7f804df2a4de7e1
amazguru <brentreidsoft@gmail.com>
2021-11-12 15:23:02 -0500

    Fix for incorrectly rendered models

--- PS2LS/ps2ls/Assets/Dme/BoneDrawCall.cs
+++ PS2LS/ps2ls/Assets/Dme/BoneDrawCall.cs
@@ -24,39 +24,39 @@ namespace ps2ls.Assets.Dme
         }
     }
 
-    public class BoneMap
+    public class BoneDrawCall
     {
         public UInt32 Unknown0 { get; private set; }
         public UInt32 BoneStart { get; private set; }
         public UInt32 BoneCount { get; private set; }
         public UInt32 Delta { get; private set; }
         public UInt32 Unknown1 { get; private set; }
-        public UInt32 BoneEnd { get; private set; }
+        public UInt32 VertexOffset { get; private set; }
         public UInt32 VertexCount { get; private set; }
-        public UInt32 Unknown2 { get; private set; }
+        public UInt32 IndexOffset { get; private set; }
         public UInt32 IndexCount { get; private set; }
 
-        private BoneMap()
+        private BoneDrawCall()
         {
         }
 
-        public static BoneMap LoadFromStream(Stream stream)
+        public static BoneDrawCall LoadFromStream(Stream stream)
         {
             if (stream == null)
                 return null;
 
             BinaryReader binaryReader = new BinaryReader(stream);
 
-            BoneMap boneMap = new BoneMap();
+            BoneDrawCall boneMap = new BoneDrawCall();
 
-            boneMap.Unknown0 = binaryReader.ReadUInt32();
+            boneMap.Unknown0 = binaryReader.ReadUInt32();//max value
             boneMap.BoneStart = binaryReader.ReadUInt32();
             boneMap.BoneCount = binaryReader.ReadUInt32();
             boneMap.Delta = binaryReader.ReadUInt32();
-            boneMap.Unknown1 = binaryReader.ReadUInt32();
-            boneMap.BoneEnd = binaryReader.ReadUInt32();
+            boneMap.Unknown1 = binaryReader.ReadUInt32();//seems to match boneCount?
+            boneMap.VertexOffset = binaryReader.ReadUInt32();
             boneMap.VertexCount = binaryReader.ReadUInt32();
-            boneMap.Unknown2 = binaryReader.ReadUInt32();
+            boneMap.IndexOffset = binaryReader.ReadUInt32();
             boneMap.IndexCount = binaryReader.ReadUInt32();
 
             return boneMap;
